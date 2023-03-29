2f5fa209dce262daa765bad9ce13f5ea35b287ae
Sabri1508 <129279294+Sabri1508@users.noreply.github.com>
2023-03-29 13:53:15 +0200

    Update main with resources and models inclusion

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 import os
 from flask import Flask
+from flask_restful import  Api
+from flask_jwt import JWT
+
+
+from db import db
+from security import authenticate,identity
+from resources.user import UserRegister
+from resources.blog import Blog , ItemList
 
 
 app             = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]   = os.environ.get('DATABASE_URL')
 
-# a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
 
+"""
+author: Sabri
+Just for testing purposes i hard coded the secret key below, it's better to store it as ENV variable 
+"""
+app.secret_key  = 'XXX156Ye54'
+api             = Api(app)
+jwt             = JWT(app,authenticate,identity)
+
+
+
+api.add_resource(Blog, '/blog/<int:id>')
+api.add_resource(ItemList, '/blogs')
+
+api.add_resource(UserRegister, '/register')
+
+
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    ''' this uses all the subclasses of the db.Model to create the tables
+    '''
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug = True)
